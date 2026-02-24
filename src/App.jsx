cc5bdb154cbbb11c744d1f6c31ba8afc4bb80616
blackshark281 <ritikgupta281@gmail.com>
2026-02-24 14:15:57 +0530

    tailwindCSS + daisyUI + navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 function App() {
   return (
     <>
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-300 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">DevTinder</a>
   </div>
   <div className="flex gap-2">
-    <div className="dropdown dropdown-end">
+    <div className="dropdown dropdown-end mx-5">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
