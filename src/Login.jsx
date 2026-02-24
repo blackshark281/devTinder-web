7549ffae6e1264cf12166270c6f04fb284be5641
blackshark281 <ritikgupta281@gmail.com>
2026-02-25 01:39:40 +0530

    basic login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [emailId, setEmailId] = useState("rohit.sharma@gmail.com");
+    const [password, setPassword] = useState("rohit@123");
+
+    const handleLogin = async () =>{
+        try{
+            const res = await axios.post("http://localhost:3000/login", 
+            {emailId, password}, {withCredentials: true}
+            );
+        }catch(err){
+            console.log(err);
+        }
+    }
+
   return (
-    <div>Login</div>
+    <div className="flex justify-center my-25">
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" value={emailId} className="input" placeholder="Email" 
+        onChange={(e) => setEmailId(e.target.value)}/>
+
+        <label className="label">Password</label>
+        <input type="password" value={password} className="input" placeholder="Password" 
+        onChange={(e) => setPassword(e.target.value)}/>
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>Login</button>
+        </fieldset>
+    </div>
   )
 }
 
