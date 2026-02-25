ea040de495367dab05051780058b156f4a15616e
blackshark281 <ritikgupta281@gmail.com>
2026-02-25 20:28:03 +0530

    authentication for routing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constant'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if(userData) return;
+    try{
+    const res = await axios.get(BASE_URL + "/profile/view", {withCredentials: true});
+    dispatch(addUser(res.data));
+    }
+  catch(err){
+    if(err.response.status === 401){
+      console.log("User not authenticated");
+      navigate("/login");
+    }else console.log(err);
+  }}
+
+  
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
         <NavBar />
