38a29a9b19285c7967fbc7981e4073f4c1298181
blackshark281 <ritikgupta281@gmail.com>
2026-03-01 11:52:25 +0530

    updated base url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 // export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
-// export const RENDER_URL = "https://devtinder-m5n9.onrender.com"
+export const BASE_URL = "https://devtinder-m5n9.onrender.com"
