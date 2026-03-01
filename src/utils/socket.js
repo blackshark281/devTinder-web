0508c3b9103d1d9dc4ba9ac34fcea23782b50f7d
blackshark281 <ritikgupta281@gmail.com>
2026-03-01 11:49:55 +0530

    updated socket 2

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,9 +2,9 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constant";
 
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
+  // if (location.hostname === "localhost") {
+  //   return io(BASE_URL);
+  // } else {
     return io("https://devtinder-m5n9.onrender.com", { path: "/api/socket.io", transports: ["websocket"] });
-  }
+  // }
 };
