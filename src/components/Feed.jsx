9bef497eaaa363556bf46bf19515c93a0519dc84
blackshark281 <ritikgupta281@gmail.com>
2026-03-05 12:08:33 +0530

    swipe feed cards

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -29,7 +29,7 @@ const Feed = () => {
   if (!feed) return;
 
   if (feed.length <= 0)
-    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
+    return <h1 className="flex justify-center my-10">Refresh page for more users!</h1>;
 
   return (
     feed && (
