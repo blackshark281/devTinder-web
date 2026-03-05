f475cd7ee4fdc02f6a8b78ee9c890e0213432293
blackshark281 <ritikgupta281@gmail.com>
2026-03-05 08:07:34 +0530

    swipe feed cards test 1

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,3 +1,6 @@
+import {motion, useMotionValue, useTransform, animate} from "framer-motion";
+import { useState } from "react";
+
 import axios from "axios";
 import { BASE_URL } from "../utils/constant";
 import { useDispatch } from "react-redux";
@@ -18,29 +21,56 @@ const UserCard = ({ user }) => {
     } catch (err) {}
   };
 
+  const [isGone, setIsGone] = useState(false);
+
+  const x = useMotionValue(0);
+
+  // Rotate based on horizontal drag
+  const rotate = useTransform(x, [-200, 200], [-20, 20]);
+
+  // Optional opacity effect
+  const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0]);
+
+  const handleDragEnd = (event, info) => {
+    if (info.offset.x > 120) {
+      // Right swipe
+      animate(x, 1000, { duration: 0.3 });
+      setIsGone(true);
+    } else if (info.offset.x < -120) {
+      // Left swipe
+      animate(x, -1000, { duration: 0.3 });
+      setIsGone(true);
+    } else {
+      // Snap back
+      animate(x, 0, { duration: 0.3 });
+    }
+  };
+
+  if (isGone) return null;
+
   return (
-    <div className="card bg-base-300 w-96 shadow-xl">
-      <figure>
-        <img src={photoUrl} alt="photo" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{firstName + " " + lastName}</h2>
-        {age && gender && <p>{age + ", " + gender}</p>}
-        <div className="card-actions justify-center my-4">
-          <button
-            className="btn btn-primary"
-            onClick={() => handleSendRequest("ignore", _id)}
-          >
-            Ignore
-          </button>
-          <button
-            className="btn btn-secondary"
-            onClick={() => handleSendRequest("interested", _id)}
-          >
-            Interested
-          </button>
+    <div className="flex justify-center mt-10">
+      <motion.div
+        className="card bg-base-300 w-96 shadow-xl cursor-grab active:cursor-grabbing"
+        style={{ x, rotate, opacity }}
+        drag="x"
+        dragConstraints={{ left: 0, right: 0 }}
+        onDragEnd={handleDragEnd}
+      >
+        <figure>
+          <img src={photoUrl} alt="photo" />
+        </figure>
+
+        <div className="card-body">
+          <h2 className="card-title">
+            {firstName + " " + lastName}
+          </h2>
+
+          {age && gender && (
+            <p>{age + ", " + gender}</p>
+          )}
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
